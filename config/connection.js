3fe6ca13068816648418e357c65eef6802b61a1c
edm1001 <edmer_franciz@yahoo.com>
2023-12-03 21:08:38 -0600

    routes working

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 // create connection to DB
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
@@ -7,4 +9,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     port: 3306
   });
   
-  module.exports = sequelize;
+module.exports = sequelize;
